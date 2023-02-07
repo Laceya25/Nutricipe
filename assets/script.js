7ace59afc964e9053ead53a33ea25a75fb08d194
Hunter Wilson <huntertheguy123@gmail.com>
2023-02-06 20:47:12 -0600

    It is making me commit because of the stuff we wrote in class

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,26 @@
 const form = document.querySelector('form')
 let searchBtn = document.querySelector("#search")
+const article = document.querySelector('article')
 
 
 // Api key and id for other api
-const nutApiKey = "08705eb334546608d328eb002f53c968"
-const nutApiId = "ff99687b"
+
 
 //input bar is wrapped in form. on event (press enter) then the function in the listener is ran
-form.addEventListener("submit", function(event) {
+ form.addEventListener("submit", function(event) {
     console.log("pressed")
     //prevents form from refreshing when submitted
     event.preventDefault();
     //retrieves the value of the input inside of element form. Basically taking what you type in bar and placing it inside of api url
     search = event.target.querySelector('input').value;
+   
     
     console.log(search)
     //Then runs the next function to load api details
     apiRequest()
+ 
 })
+
 
 // function to fetch data from api
 const recApiKey = "b66c48f1da5cbf78d437f8b08aa18632"
@@ -33,6 +36,8 @@ async function apiRequest(){
     console.log(data)
 }
 
+
 function createCard() {
 
 }
+
