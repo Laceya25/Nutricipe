d69c262d261bc7befc8b17fb57b2e2066df1891f
cam273364 <cam273364@gmail.com>
2023-02-08 14:32:58 -0500

    added function to add search history

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ let searchBtn = document.querySelector("#search")
 const article = document.querySelector('article')
 
 
-// Api key and id for other api
 
 
+let searchHistory = [] 
 //input bar is wrapped in form. on event (press enter) then the function in the listener is ran
  form.addEventListener("submit", function(event) {
     console.log("pressed")
@@ -13,19 +13,34 @@ const article = document.querySelector('article')
     event.preventDefault();
     //retrieves the value of the input inside of element form. Basically taking what you type in bar and placing it inside of api url
     search = event.target.querySelector('input').value;
-   
-    
-    console.log(search)
+    searchHistory.push(search)
+    //when user searches add that search to history list
+    addItemToSearchHistory(search)
+
+    console.log(searchHistory)
     //Then runs the next function to load api details
     apiRequest()
     apiRequest2()
  
 })
 
+//function to add search item to html class for search history
+function addItemToSearchHistory (item) {
+    //grabs search history items element defined in index html/dom (ul)
+    var searchHistoryContainer = document.querySelector('#search-history-items')
+    //creating new li element which will have search item as text
+    var searchHistoryItemElement = document.createElement("li")
+    //setting text content of new element
+    searchHistoryItemElement.textContent = item
+    //adding new item to search history element
+    searchHistoryContainer.appendChild(searchHistoryItemElement)
+}
 
 // function to fetch data from api
+// Api key and id for other api
 const recApiKey = "b66c48f1da5cbf78d437f8b08aa18632"
 const recApiId = "965f718b"
+
 async function apiRequest(){
     //api url
     const recipeURL = `https://api.edamam.com/search?q=${search}&app_id=${recApiId}&app_key=${recApiKey}`;
@@ -46,6 +61,7 @@ async function apiRequest(){
 
 
 
+ 
 function createCard() {
 
 }
